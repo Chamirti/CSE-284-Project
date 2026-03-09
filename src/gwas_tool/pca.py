f4701a67261b8c463a258e652973aacb484c9cc6
Chamirti <103818159+Chamirti@users.noreply.github.com>
2026-03-08 21:46:39 -0700

    Update pca.py

--- src/gwas_tool/pca.py
+++ src/gwas_tool/pca.py
@@ -1,13 +1,22 @@
 import subprocess
+import pandas as pd
 
 def compute_pcs(genotype_prefix, output_file, num_pcs=10):
-    """
-    Uses PLINK to compute top principal components for population structure.
-    """
-    # Run PLINK PCA
+
+    prefix = str(output_file).replace(".txt","")
+
     subprocess.run([
         "plink",
         "--bfile", genotype_prefix,
         "--pca", str(num_pcs),
-        "--out", output_file.with_suffix('')
+        "--out", prefix
     ])
+
+    # Load eigenvec file
+    eigenvec_file = prefix + ".eigenvec"
+    df = pd.read_csv(eigenvec_file, delim_whitespace=True, header=None)
+
+    # Remove FID and IID columns
+    pcs = df.iloc[:, 2:]
+
+    pcs.to_csv(output_file, sep=" ", index=False, header=False)
