91adfaa8a9680259326bb8888568d553b292bb9f
Chamirti <103818159+Chamirti@users.noreply.github.com>
2026-03-09 10:11:22 -0700

    Update gwas.py

--- src/gwas_tool/gwas.py
+++ src/gwas_tool/gwas.py
@@ -2,11 +2,18 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 
-def run_gwas_linear(genotype_prefix, phenotype_file, covariate_file, output_file):
+
+def run_gwas(genotype_prefix, phenotype_file, covariate_file, output_file, model_type="linear"):
     """
-    Run linear GWAS for each SNP, correcting for covariates (PCs).
+    Run GWAS for each SNP using either linear or logistic regression.
+    
+    model_type:
+        "linear"   -> continuous phenotype
+        "logistic" -> binary phenotype (0/1)
     """
+
     geno_file = genotype_prefix + ".raw"
+
     geno_df = pd.read_csv(geno_file, delim_whitespace=True)
     pheno_df = pd.read_csv(phenotype_file, delim_whitespace=True, header=None)
     pcs_df = pd.read_csv(covariate_file, delim_whitespace=True, header=None)
@@ -18,12 +25,33 @@ def run_gwas_linear(genotype_prefix, phenotype_file, covariate_file, output_file
 
     for snp in geno_df.columns[6:]:
         snp_values = geno_df[snp].values.reshape(-1, 1)
+
+        # Combine SNP + covariates
         X = np.hstack([snp_values, covariates])
         X = sm.add_constant(X)
-        model = sm.OLS(y, X).fit()
-        beta = model.params[1]
-        pval = model.pvalues[1]
-        results.append({"SNP": snp, "beta": beta, "pval": pval})
+
+        try:
+            if model_type == "linear":
+                model = sm.OLS(y, X).fit()
+
+            elif model_type == "logistic":
+                model = sm.Logit(y, X).fit(disp=0)
+
+            else:
+                raise ValueError("model_type must be 'linear' or 'logistic'")
+
+            beta = model.params[1]
+            pval = model.pvalues[1]
+
+        except Exception:
+            beta = np.nan
+            pval = np.nan
+
+        results.append({
+            "SNP": snp,
+            "beta": beta,
+            "pval": pval
+        })
 
     results_df = pd.DataFrame(results)
     results_df.to_csv(output_file, index=False)
